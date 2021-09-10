4ef0d288b1811036d8ae05257e6e4bf0969eee70
Tobias Ribizel <ribizel@kit.edu>
2021-09-10 14:35:35 +0200

    Update bot-pr-format-base.sh

--- .github/bot-pr-format-base.sh
+++ .github/bot-pr-format-base.sh
@@ -2,7 +2,6 @@
 
 source .github/bot-pr-base.sh
 
-EXTENSION_REGEX='\.(cuh?|hpp|hpp\.inc?|cpp)$'
 FORMAT_HEADER_REGEX='^(benchmark|core|cuda|hip|include/ginkgo/core|omp|reference|dpcpp)/'
 FORMAT_REGEX='^(common|examples|test_install)/'
 

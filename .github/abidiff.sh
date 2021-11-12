1411c4619a8aa9b47306a2eaf1396e247d04ac5e
Tobias Ribizel <ribizel@kit.edu>
2021-11-12 15:28:11 +0100

    fix JSON newline output

--- .github/abidiff.sh
+++ .github/abidiff.sh
@@ -6,4 +6,4 @@ popd
 set -xe
 bot_delete_comments_matching "Error: This PR changes the Ginkgo ABI"
 
-abidiff build-old/lib/libginkgod.so build-new/lib/libginkgod.so &> abi.diff || bot_error "This PR changes the Ginkgo ABI:\n\`\`\`\n$(head -n2 abi.diff)\n\`\`\`\nFor details check the full ABI diff under **Artifacts** [here]($JOB_URL)"
+abidiff build-old/lib/libginkgod.so build-new/lib/libginkgod.so &> abi.diff || bot_error "This PR changes the Ginkgo ABI:\n\`\`\`\n$(head -n2 abi.diff | sed 's/$/\\\\n/' | tr -d '\n')\n\`\`\`\nFor details check the full ABI diff under **Artifacts** [here]($JOB_URL)"

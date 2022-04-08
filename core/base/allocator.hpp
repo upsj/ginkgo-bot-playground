7b79021e07c1b27f112722fc653fe153fefc5025
Tobias Ribizel <ribizel@kit.edu>
2022-04-08 17:14:20 +0200

    Update allocator.hpp

--- core/base/allocator.hpp
+++ core/base/allocator.hpp
@@ -2,7 +2,6 @@
 Copyright (c) 2017-2021, the Ginkgo authors
 All rights reserved.
 
-
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
 are met: 

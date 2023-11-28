a2510ce73d0f9a9342797fdad9bd1caab121be46
Tobias Ribizel <ribizel@kit.edu>
2023-11-28 13:35:36 +0100

    Update bot-base.sh

--- .github/bot-base.sh
+++ .github/bot-base.sh
@@ -6,7 +6,13 @@ API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 
 api_get() {
-  curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" "$1"
+  if [[ "$RUNNER_DEBUG" == "1" ]]; then
+      curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" "$1" > $RUNNER_TEMP/output.json
+      cat $RUNNER_TEMP/output.json 1>&2
+      cat $RUNNER_TEMP/output.json
+  else
+      curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" "$1"
+  fi
 }
 
 api_post() {
